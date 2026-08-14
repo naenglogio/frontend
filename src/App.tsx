abe7a11270494672cc606cfc0f0e556a551cfaef
songwoohee <songwoohee@naver.com>
2026-08-14 23:29:08 +0900

    feat: 비로그인 사용자 접근 제한 라우트 가드(RequireAuth) 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
-import { LoginPage, SignupPage } from './components/pages';
+import { LoginPage, ProfilePage, SignupPage } from './components/pages';
+import { RequireAuth } from './components/RequireAuth';
 
 function App() {
   return (
@@ -8,6 +9,15 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        {/* 로그인해야 볼 수 있는 페이지는 아래처럼 <RequireAuth>로 감싸서 등록할 것 */}
+        <Route
+          path="/profile"
+          element={
+            <RequireAuth>
+              <ProfilePage />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
